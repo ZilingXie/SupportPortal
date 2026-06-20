8bea5eb5bed6794fd54bc4c3f9233e6b8d312892
ZilingXie <xieziling88@gmail.com>
2026-06-21 00:20:59 +0800

    fix: keep graphrag reranker optional

--- vendor/cusmem/graphiti_core/cross_encoder/__init__.py
+++ vendor/cusmem/graphiti_core/cross_encoder/__init__.py
@@ -1,4 +1,11 @@
 from .client import CrossEncoderClient
-from .bge_reranker_client import BGERerankerClient
 
 __all__ = ['CrossEncoderClient', 'BGERerankerClient']
+
+
+def __getattr__(name: str):
+    if name == 'BGERerankerClient':
+        from .bge_reranker_client import BGERerankerClient
+
+        return BGERerankerClient
+    raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
